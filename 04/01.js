4a4ee359527d810a4f6f1870ec7488b76d496f25
Storm <yannic.freson1@gmail.com>
2022-12-05 15:39:17 +0100

    Day 4 complete

--- 04/01.js
+++ 04/01.js
@@ -1,4 +1,1 @@
-let total = 0;
-require("fs").readFileSync("./input.txt", "utf8").trim().split("\n").map((line) => {(parseInt(line.split(",")[0].split("-")[0]) <= parseInt(line.split(",")[1].split("-")[0]) && parseInt(line.split(",")[0].split("-")[1]) >= parseInt(line.split(",")[1].split("-")[1])) || (parseInt(line.split(",")[1].split("-")[0]) <= parseInt(line.split(",")[0].split("-")[0]) && parseInt(line.split(",")[1].split("-")[1]) >= parseInt(line.split(",")[0].split("-")[1])) ? total++ : null});
-
-console.log(total);
+console.log(require("fs").readFileSync("./input.txt", "utf8").trim().split("\n").map((line) =>(parseInt(line.split(",")[0].split("-")[0]) <=parseInt(line.split(",")[1].split("-")[0]) &&parseInt(line.split(",")[0].split("-")[1]) >= parseInt(line.split(",")[1].split("-")[1])) || (parseInt(line.split(",")[1].split("-")[0]) <= parseInt(line.split(",")[0].split("-")[0]) && parseInt(line.split(",")[1].split("-")[1]) >= parseInt(line.split(",")[0].split("-")[1])) ? 1 : 0).reduce((a, b) => a + b));
