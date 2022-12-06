60801bd01686dc0b67d7b6ae1f8f28f240dd0466
Storm <yannic.freson1@gmail.com>
2022-12-06 13:05:42 +0100

    Day 6 complete

---
+++ 06/02.js
@@ -0,0 +1,16 @@
+const input = require("fs").readFileSync("./input.txt", "utf8").trim();
+
+let start = 0;
+let end = 14;
+let rollingWindow = input.slice(start, end).split("");
+
+while (true) {
+  if (new Set(rollingWindow).size === rollingWindow.length) {
+    console.log(end);
+    break;
+  } else {
+    start++;
+    end++;
+    rollingWindow = input.slice(start, end).split("");
+  }
+}
